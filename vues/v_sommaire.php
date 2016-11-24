c2a098226f6c3c8f58c6f623bf5b99656f3fc1b8
SarahGalichon <sarah.galichon@gmail.com>
2016-11-24 08:31:06 +0100

    modification1

--- vues/v_sommaire.php
+++ vues/v_sommaire.php
@@ -21,6 +21,10 @@
            <li class="smenu">
               <a href="index.php?uc=affectationTablette&action=affectationTablette" title="Affectations des tablettes">Affectations des tablettes</a>
            </li>
+	   <li class="smenu">
+              <a href="index.php?uc=afficherhistorique&action=afficherhistorique" title="Afficher historique pour visiteur">Afficher historique pour visiteur</a>
+           </li>
+
  	   <li class="smenu">
               <a href="index.php?uc=connexion&action=deconnexion" title="Se déconnecter">Déconnexion</a>
            </li>
